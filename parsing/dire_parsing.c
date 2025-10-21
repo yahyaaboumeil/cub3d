c64c6b380a543918a47f35be874f5ebfb297b357
Yahya Aboumeil <yaboumei@c1r5p8.1337.ma>
2025-10-21 22:27:05 +0100

    done

--- parsing/dire_parsing.c
+++ parsing/dire_parsing.c
@@ -16,40 +16,109 @@ int open_file(char *file_name, int fd)
     return fd; 
 }
 
-char *check_line(char *line)
+int skip_ind_space(char *line)
 {
-    if (ft_strncmp(line, "NO", 2))
+    int i;
+
+    i  = 1;
+    while (line[i] && line[i] != ' ')
+        i++;
+
+    return i;
+}
+
+bool    check_path(char *line)
+{
+    int i;
+    char *path;
+    int fd;
+
+
+    i = skip_ind_space(line);
+    if (i == ft_strlen(line))
+        return false;
+    path = line+i;
+    fd  = open(path, O_RDONLY);
+    if (fd == -1)
     {
-        
+        printf("\n the file is't exist\n"); 
+        return false;
+    }
+    return true;
+}
+
+
+
+bool check_line(char *line, t_direction *directions)
+{
+
+    if (ft_strncmp(line, "NO", 3))
+    {
+        directions->no_count++;
+        if (check_path(line))
+            return false;
     } 
     else if (ft_strncmp(line, "SO", 2))
     {
-
+        directions->so_count++;
+        if (check_path(line))
+            return false;
     }
     else if (ft_strncmp(line, "WE", 2))
     {
-        
+        directions->we_count++;
+        if (check_path(line))
+            return false;
     }
     else if (ft_strncmp(line, "EA", 2))
     {
-
+        directions->ea_count++;
+        if (check_path(line))
+            return false;
     }
+    else
+        return false;
+    return true;
 }
 
+char *get_line(int fd)
+{
+    char *line;
+
+    line = get_next_line(fd); 
+    while (ft_strncmp(line, "\n", ft_strlen(line)))
+    {
+        free(line);
+        line = get_next_line(fd); 
+    }
+
+    return line;
+}
 
 char **diretion_pasing(char *file_name)
 {
     int fd;
+    int i;
     char *line;
     char **lines;
+    t_direction dire;
 
     line = NULL;
+    
     fd = open_file(file_name, fd);
     if (fd == -1)
     {
         printf("\nError\nCannot open file\n");
         return NULL;
     }
-    line = get_next_line(fd); 
-
+    i = -1;
+    while (++i)
+    { 
+        line = get_line(fd);
+        if (!line)
+            return lines;
+        if (!check_line(line), &dire)
+            return NULL;
+        free(line);
+    }
 }
